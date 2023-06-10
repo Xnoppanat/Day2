f29f008c8ee803770702d058730395d1633f3c91
Noppanat <nine.noppanat@gmail.com>
2023-06-10 16:11:53 +0700

    sort

---
+++ sortnum.py
@@ -0,0 +1,12 @@
+numbers = [1 , 3 , 5 , 7 , 9 , 2 , 4 , 6 , 8 , 10]
+
+print (numbers)
+
+numbers.sort()
+
+print(numbers)
+
+numbers.sort(reverse = True)
+
+print(numbers)
+
---
+++ sortstr.py
@@ -0,0 +1,11 @@
+names = ["bouya" , "aot" , "nine" , "nuna"]
+
+print (names)
+
+names.sort()
+
+print (names)
+
+names.sort(reverse = True)
+
+print (names)
